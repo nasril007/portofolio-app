330a869d91bca6bc2d05fd5bab823e77008ff78f
nasril007 <nasrilcourier@gmail.com>
2021-04-15 01:22:52 +0700

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,13 +6,13 @@ import { faBars} from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className ="container">
 
         <div className="container-fluid">
           <a className="navbar-brand" href="#"><img className="logo" src={logo} alt="logo..."/></a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <FontAwesomeIcon icon={faBars} style={{color: "#ffd700" }} />
+            <FontAwesomeIcon icon={faBars} style={{color: "#4c5270" }} />
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav ml-auto mb-2 mb-lg-0">
